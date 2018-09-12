820fc022e737d68dd1fb4b30800b253d1bceacb5
Armando Padilla <armando_padilla_81@yahoo.com>
2018-09-12 07:41:53 -0700

    wallet and contest

---
+++ modules/contest/routes/patchUpdateContest.js
@@ -0,0 +1,62 @@
+/**
+ * Update a contest - admin only
+ *
+ */
+const Joi = require('joi');
+const ObjectID = require('mongodb').ObjectId;
+const { response } = require('../../../utils');
+
+const handler = async (req, res) => {
+  const { contestId } = req.params;
+  const { db } = res.context.config;
+  const {
+    title,
+    startDateTime,
+    endDateTime,
+    pot,
+    streamURL,
+    status
+  } = req.body;
+
+  const updateObj = {
+    title,
+    startDateTime,
+    endDateTime,
+    pot,
+    streamURL,
+    status
+  };
+
+  try {
+    const data = await db.collection('contests').updateOne(
+      { id: ObjectID(contestId) },
+      {$set: updateObj}
+    );
+
+    if (data.matchedCount) return response.success(insertObj);
+    return response.error();
+  } catch(error) {
+    console.log(error);
+    return response.error(error);
+  }
+};
+
+
+module.exports = fastify => fastify.route({
+  method: 'PATCH',
+  url: '/:contestId',
+  handler,
+  schema: {
+    body: {
+      title: Joi.string().required(),
+      startDateTime: Joi.string().required(),
+      endDateTime: Joi.string().required(),
+      pot: Joi.number(),
+      streamURL: Joi.string().required()
+    }
+  },
+  schemaCompiler: schema => data => Joi.validate(data, schema),
+  config: {
+    db: fastify.mongo.db // This seems off.
+  }
+});
