f5ca80a737a45269d927a56344b9b5af5f810d14
Armando Padilla <armando_padilla_81@yahoo.com>
2018-11-16 07:02:40 -0800

    integration test for contest creation.  updated docs

--- modules/contest/routes/postCreateContest.js
+++ modules/contest/routes/postCreateContest.js
@@ -2,6 +2,7 @@
  * Create a new contest - admin only
  * status = pending | in_progress | distributing_pot | paused | completed
  *
+ * @todo - Auth
  */
 const { response } = require('../../../utils');
 const { db: collection } = require('../../../config');
@@ -23,7 +24,7 @@ const handler = async (req, res) => {
     endDateTime,
     pot,
     streamURL,
-    status, // active, completed
+    status,
     participants: [], // Initially empty
     entryFee: 0, // Replace with some default
   };
@@ -32,7 +33,9 @@ const handler = async (req, res) => {
     const data = await db.collection(collection.CONTEST_NAME).insertOne(insertObj);
 
     if (data.insertedCount) return response.success(insertObj);
-    return response.error();
+
+    // When is this triggered???
+    return response.error('Could not create contest.  Unknown error', 400);
   } catch (error) {
     return response.error(error);
   }
@@ -55,10 +58,10 @@ module.exports = fastify => fastify.route({
         endDateTime: { type: 'string', format: 'date-time', description: 'End date time of contest' },
         pot: { type: 'number', description: 'total amount in pot' },
         streamURL: { type: 'string', description: 'Streaming service URL. Used to stream video.' },
-        status: { type: 'string', description: 'Contest status' },
-      }
+        status: { type: 'string', description: 'Contest status', enum: ['active', 'pending', 'in_progress', 'distributing_pot', 'paused', 'completed' ] }
+      },
+      required: ['title', 'startDateTime', 'endDateTime', 'streamURL', 'status'],
     },
-    required: ['title', 'startDateTime', 'endDateTime', 'streamURL', 'status'],
     response: {
       200: {
         description: 'Successful response',
@@ -73,9 +76,27 @@ module.exports = fastify => fastify.route({
               endDateTime: { type: 'string', format: 'date-time', description: 'End date time of contest' },
               pot: { type: 'number', description: 'total amount in pot' },
               streamURL: { type: 'string', description: 'Streaming service URL. Used to stream video.' },
-              status: { type: 'string', description: 'Contest status' },
+              status: { type: 'string', description: 'Contest status', enum: ['active', 'pending', 'in_progress', 'distributing_pot', 'paused', 'completed' ] },
             }
           }
+        }
+      },
+      400: {
+        description: 'Bad Request',
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number' },
+          error: { type: 'string' },
+          message: { type: 'string' }
+        }
+      },
+      500: {
+        description: 'Internal Server Error',
+        type: 'object',
+        properties: {
+          statusCode: { type: 'number' },
+          error: { type: 'string' },
+          message: { type: 'string' }
         }
       }
     }
---
+++ tests/integration/contests/postCreateContest.js
@@ -0,0 +1,77 @@
+const should = require('should');
+const supertest = require('supertest');
+const fastify = require('../../../server');
+const { db: collection } = require('../../../config/index');
+
+describe ('Create Contest', () => {
+
+  let db;
+  let contestId;
+  let contestObj = {
+    title: 'CONTEST',
+    startDateTime: new Date(),
+    endDateTime: new Date(),
+    pot: 0,
+    streamURL: 'https://www.twitch.tv/riotgames',
+    status: 'active'
+  };
+
+  before( async () => {
+    await fastify.ready();
+    db = fastify.mongo.db;
+  });
+
+  after(async () => {
+    await db.collection(collection.CONTEST_NAME).deleteMany({ });
+  });
+
+  it('should successfully create contest and return correct payload.', async () => {
+    const response = await supertest(fastify.server)
+      .post('/contest')
+      .send(contestObj)
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8');
+
+    response.body.should.have.only.property('data');
+    const data = response.body.data;
+
+    data.should.have.property('title', contestObj.title);
+    data.should.have.property('startDateTime');
+    data.should.have.property('endDateTime');
+    data.should.have.property('pot', contestObj.pot);
+    data.should.have.property('streamURL', contestObj.streamURL);
+    data.should.have.property('status', contestObj.status);
+    data.should.have.property('_id');
+  });
+
+  it ('should fail when required parameters are not present.', async () => {
+    // Missing title
+    let obj = Object.assign({}, contestObj);
+    delete obj.title;
+
+    let response = await supertest(fastify.server).post('/contest').send(obj).expect(400);
+    response.body.message.should.equal('body should have required property \'title\'');
+
+    // Missing startDateTime
+    obj = Object.assign({}, contestObj);
+    delete obj.startDateTime;
+
+    response = await supertest(fastify.server).post('/contest').send(obj).expect(400);
+    response.body.message.should.equal('body should have required property \'startDateTime\'');
+
+    // Missing endDateTime
+    obj = Object.assign({}, contestObj);
+    delete obj.endDateTime;
+
+    response = await supertest(fastify.server).post('/contest').send(obj).expect(400);
+    response.body.message.should.equal('body should have required property \'endDateTime\'');
+
+    // Missing streamURL
+    obj = Object.assign({}, contestObj);
+    delete obj.streamURL;
+
+    response = await supertest(fastify.server).post('/contest').send(obj).expect(400);
+    response.body.message.should.equal('body should have required property \'streamURL\'');
+  });
+
+});
